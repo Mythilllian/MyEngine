bcbca90a96e979844a793a4f8a3b85aa2b1e18dd
Leois <Leois@DESKTOP-OUPJE97>
2023-10-04 18:31:42 -0400

    Add project files.

---
+++ GameEngine/RenderObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace GameEngine
+{
+    public class RenderObject
+    {
+        public Bitmap sprite;
+        public Vector position;
+        public Vector size;
+        public float rotation { 
+            get { return rotation; } 
+            set { rotation = (value % 360) + (int)(value / 360) + (value < 0 ? 360 : 0); } 
+        }
+    }
+}
---
+++ GameEngine/Vector.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine
+{
+    public struct Vector
+    {
+        //Vectors hold positions
+        //Only x and y because this is 2d app
+        double x;
+        double y;
+
+        public Vector(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        //Operators
+        public static Vector operator +(Vector a) => a;
+        public static Vector operator -(Vector a) => new Vector(-a.x, -a.y);
+        public static Vector operator +(Vector a, Vector b) => new Vector(a.x + b.x, a.y + b.y);
+        public static Vector operator -(Vector a, Vector b) => new Vector(a.x - b.x, a.y - b.y);
+        public static Vector operator *(Vector a, Vector b) => new Vector(a.x * b.x, a.y * b.y);
+        public static Vector operator /(Vector a, Vector b) => new Vector(a.x / b.x != 0 ? b.x : throw new DivideByZeroException(), a.y / b.y != 0 ? b.y : throw new DivideByZeroException());
+        public static Vector operator %(Vector a, Vector b) => new Vector(a.x % b.x, a.y % b.y);
+    }
+}
